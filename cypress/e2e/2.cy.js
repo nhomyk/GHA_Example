f02cd704d5b74e2aceb01b677ecfaa24166a04d0
“nicholashomyk” <nickhomyk@gmail.com>
2024-06-05 16:31:32 -0400

    17 - edited integration-tests.yml

--- cypress/e2e/2.cy.js
+++ cypress/e2e/2.cy.js
@@ -15,41 +15,6 @@ it('Homepage - Assert load', () => {
 })
 })
 
-it('Homepage - Load page, verify elements', () => {
-    cy.visit('/')
-    const createObj = new Objects
-
-    // Verify header links
-    createObj.testObjects.leftheaderbutton().contains('About')
-    createObj.testObjects.leftheaderbutton().contains('Store')
-    createObj.testObjects.rightheaderbutton().contains('Gmail')
-    createObj.testObjects.rightheaderbutton().contains('Images')
-    createObj.testObjects.GoogleAppsButton()
-    createObj.testObjects.GoogleLogo()
-
-    // Verify page buttons 
-    cy.get('input[value="Google Search', { timeout: 60000 })
-    cy.get('input', { timeout: 60000 }).contains('Feeling Lucky')
-
-    // Verify footer links
-
-    createObj.testObjects.footerbutton().contains('Advertising')
-    createObj.testObjects.footerbutton().contains('Business')
-    createObj.testObjects.footerbutton().contains('How Search works')
-    createObj.testObjects.footerbutton().contains('Privacy')
-    createObj.testObjects.footerbutton().contains('Terms')
-    createObj.testObjects.footerSettingsbutton().contains('Settings')
-
-
-
-    // Click image upload button, verify dropdown, cancel
-    // cy.get('div[aria-label="Search by image"]', { timeout: 60000 }).click()
-   // cy.get('span[id="dragText"]', { timeout: 60000 })
-    // cy.get('div[id="maskedImage"]', { timeout: 60000 })
-
-      
-  })
-
 it('Homepage - Search - Verify single search and result ', () => {
     cy.visit('/')
     const createObj = new Objects
